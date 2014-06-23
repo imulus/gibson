cf508fa5548bc0282366691665745a4e9e7769ac
Taylor Smith <mail@taylorsmith.net>
2014-06-23 13:22:21 -0600

    Run `brew doctor` after install, add other taps

--- lib/brew/install.sh
+++ lib/brew/install.sh
@@ -2,3 +2,8 @@ sudo mkdir -p /usr/local /opt/homebrew-cask
 sudo chown -R `whoami` /usr/local /opt/homebrew-cask
 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+brew doctor
+
+brew tap caskroom/cask
+brew tap thoughtbot/formulae
