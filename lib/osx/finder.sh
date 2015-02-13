0c8f8f4420ac46dbce789d19b604e99938f0fbf5
Taylor Smith <mail@taylorsmith.net>
2015-02-13 10:18:54 -0700

    Add more OS X customization

---
+++ lib/osx/finder.sh
@@ -0,0 +1,10 @@
+# Hide icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+killall Finder
