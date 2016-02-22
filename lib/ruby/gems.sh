f84a4e7f2b03ef2a4b11f1756f531eea5a789cf3
Taylor Smith <mail@taylorsmith.net>
2016-02-22 09:55:08 -0700

    Update ruby version

--- lib/ruby/gems.sh
+++ lib/ruby/gems.sh
@@ -1,10 +1,4 @@
-rbenv shell 2.0.0-p598
-
-gem update --system
-gem install bundler
-gem install rails
-
-rbenv shell 2.2.0
+rbenv shell 2.3.0
 
 gem update --system
 gem install bundler
--- lib/ruby/install.sh
+++ lib/ruby/install.sh
@@ -1,8 +1,5 @@
 eval "$(rbenv init -)"
 
-rbenv install 2.0.0-p598
-rbenv install 2.2.0
-
-rbenv global 2.2.0
-
+rbenv install 2.3.0
+rbenv global 2.3.0
 rbenv rehash
